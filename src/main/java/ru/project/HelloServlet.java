c08f1b6b6513d76ff427cc3b6cad62d35c98ba30
Кудинов Валентин Владимирович <valentin.kudinov@magenta-technology.com>
2021-12-13 11:05:24 +0400

    Пишем в консоль и файл.

--- src/main/java/ru/project/HelloServlet.java
+++ src/main/java/ru/project/HelloServlet.java
@@ -1,6 +1,9 @@
 package ru.project;
 
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -12,11 +15,30 @@ import java.io.PrintWriter;
 @WebServlet("/hello")
 public class HelloServlet extends HttpServlet {
 
+    Logger logger = LogManager.getLogger(HelloServlet.class);
+
+    public HelloServlet() {
+        super();
+    }
+
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.setContentType("text/html");
-        PrintWriter printWriter = resp.getWriter();
-        printWriter.write("Hello!");
-        printWriter.close();
+    protected void doGet(HttpServletRequest request,
+                         HttpServletResponse response) throws ServletException, IOException {
+
+        response.setContentType("text/html");
+        PrintWriter writer = response.getWriter();
+        writer.println("<h1>Hello World Servlet on WildFly</h1>");
+
+        for (int i=0; i < 100; i++){
+            logger.warn("Hello world Log4j2 on WildFly");
+            logger.warn("Request URI {} - Session Id {}.", request.getRequestURI(), request.getSession().getId());
+        }
+
+        writer.close();
+    }
+
+    protected void doPost(HttpServletRequest request,
+                          HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
     }
 }
